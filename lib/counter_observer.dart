b38a28b82a413c491a1b7f626694340671cd269c
Lucas Lima da Silva <lukesilva.dev@gmail.com>
2021-09-12 19:43:36 -0300

    fix onChange

--- lib/counter_observer.dart
+++ lib/counter_observer.dart
@@ -2,7 +2,8 @@ import 'package:bloc/bloc.dart';
 
 class CounterObserver extends BlocObserver {
   @override
-  void onChange(bloc, change) {
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
     print("${bloc.runtimeType} $change");
   }
 }
